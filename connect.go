239370110d4be9b590e745ff6a1a2f962e81ebab
Tim <Timothy.MGregg@gmail.com>
2021-04-01 18:05:41 -0400

    Connection logic is funky

--- connect.go
+++ connect.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"time"
+)
 
 func main () {
-	fmt.Println("Hola, world.")
+	// Atlas connection
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(
+	"mongodb+srv://tim:<password>@cluster0.kkwum.mongodb.net/the_db?retryWrites=true&w=majority",
+	))
+	if err != nil { panic(err) }
+
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected and pinged.")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/TimothyGregg/offsite-mangoes
 
 go 1.16
+
+require go.mongodb.org/mongo-driver v1.5.1 // indirect
