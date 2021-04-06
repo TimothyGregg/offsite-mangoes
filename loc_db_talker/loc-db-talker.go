3b1d15e2584d5398df072f095d3c43a3150b4964
Tim <Timothy.MGregg@gmail.com>
2021-04-06 17:59:50 -0400

    I am very proud of myself. This now reads any table, no problem

--- loc_db_talker/loc-db-talker.go
+++ loc_db_talker/loc-db-talker.go
@@ -2,21 +2,26 @@ package loc_db_talker
 
 import (
 	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
-	"log"
 	"io/ioutil"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type PlaylistSong struct {
-	PlaylistID string
-	SongID string
+	PlaylistID string "bson:\"PlaylistID\""
+	SongID     string "bson:\"SongID\""
 }
 
 func Songs_Table_Reader(c chan PlaylistSong) {
-	db, err := sql.Open("mysql", "root:" + getPass() + "@(localhost:3306)/spotify?parseTime=true")
-
+	db, err := sql.Open("mysql", "root:"+getPass()+"@(localhost:3306)/spotify?parseTime=true")
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error())
 	}
 
 	query := "SELECT PlaylistID, SongID FROM playlists_songs"
@@ -49,4 +54,41 @@ func getPass() (file_data string) {
 
 	file_data = string(data)
 	return
+}
+
+func Table_Reader(c chan []interface{}, table_name string) {
+	db, err := sql.Open("mysql", "root:"+getPass()+"@(localhost:3306)/spotify?parseTime=true")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	rows, err := db.Query("SELECT * FROM " + table_name)
+
+	columns, err := rows.Columns()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// This is a wacky workaround to meet the expected input type of rows.Scan()
+	values := make([]sql.RawBytes, len(columns))
+	scanArgs := make([]interface{}, len(values))
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+
+	for rows.Next() {
+		err = rows.Scan(scanArgs...)
+		if err != nil {
+			panic(err.Error())
+		}
+		// This is dumb. I should really pass back "values" but the compiler doesn't like that.
+		c <- scanArgs
+	}
+
+	close(c)
 }
--- main.go
+++ main.go
@@ -2,25 +2,29 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"io/ioutil"
 	"log"
 	"net/url"
 	"time"
+
 	"github.com/TimothyGregg/offsite-mangoes/loc_db_talker"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-func main () {
+func main() {
 	// Atlas connection
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(
-	"mongodb+srv://tim:" + url.QueryEscape(getPass()) + "@cluster0.kkwum.mongodb.net/the_db?retryWrites=true&w=majority",
+		"mongodb+srv://tim:"+url.QueryEscape(getPass())+"@cluster0.kkwum.mongodb.net/the_db?retryWrites=true&w=majority",
 	))
-	if err != nil { panic(err) }
+	if err != nil {
+		panic(err)
+	}
 
 	defer func() {
 		if err = client.Disconnect(ctx); err != nil {
@@ -34,10 +38,21 @@ func main () {
 
 	fmt.Println("Successfully connected and pinged.")
 
-	c := make(chan loc_db_talker.PlaylistSong)
-	go loc_db_talker.Songs_Table_Reader(c)
-	for ps := range c {
-		fmt.Println(ps.PlaylistID + " : " + ps.SongID)
+	c := make(chan []interface{})
+	// goroutine for adding things to the channel. Without this, the loop hangs forever
+	go loc_db_talker.Table_Reader(c, "tracks")
+	var outstring string
+	for interface_array := range c {
+		outstring = ""
+		pointers := make([]*sql.RawBytes, len(interface_array))
+		for i, pointer := range interface_array {
+			// Extraction of value form interface requires type assertion. 
+			pointers[i] = pointer.(*sql.RawBytes)
+		}
+		for _, p := range pointers {
+			outstring += string(*p) + " / "
+		}
+		fmt.Println(outstring)
 	}
 }
 
