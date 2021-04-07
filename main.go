50186e5276bedceabad4bcda155d674b3928cfca
Tim <Timothy.MGregg@gmail.com>
2021-04-07 17:35:07 -0400

    Lots of reafing APIs

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/TimothyGregg/offsite-mangoes/loc_db_talker"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -22,19 +23,13 @@ func main() {
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(
 		"mongodb+srv://tim:"+url.QueryEscape(getPass())+"@cluster0.kkwum.mongodb.net/the_db?retryWrites=true&w=majority",
 	))
-	if err != nil {
-		panic(err)
-	}
+	if err != nil { panic(err) }
 
 	defer func() {
-		if err = client.Disconnect(ctx); err != nil {
-			panic(err)
-		}
+		if err = client.Disconnect(ctx); err != nil { panic(err) }
 	}()
 
-	if err := client.Ping(ctx, readpref.Primary()); err != nil {
-		panic(err)
-	}
+	if err := client.Ping(ctx, readpref.Primary()); err != nil { panic(err) }
 
 	fmt.Println("Successfully connected and pinged.")
 
@@ -42,17 +37,30 @@ func main() {
 	// goroutine for adding things to the channel. Without this, the loop hangs forever
 	go loc_db_talker.Table_Reader(c, "tracks")
 	var outstring string
+	var field_names []string
+	var input []string
+	var filter bson.D
+	var update bson.D
+	initial := true
 	for interface_array := range c {
-		outstring = ""
-		pointers := make([]*sql.RawBytes, len(interface_array))
-		for i, pointer := range interface_array {
-			// Extraction of value form interface requires type assertion. 
-			pointers[i] = pointer.(*sql.RawBytes)
+		if initial {
+			field_names = make([]string, len(interface_array))
+			input = make([]string, len(interface_array))
+			initial = false
+			for i, pointer := range interface_array {
+				// Extraction of value from interface requires type assertion. 
+				field_names[i] = string(*pointer.(*sql.RawBytes))
+			}
+			filter = bson.D{field_names}
+		} else {
+			for i, pointer := range interface_array {
+				// Extraction of value from interface requires type assertion. 
+				input[i] = pointer.(*sql.RawBytes)
+			}
+			for _, p := range pointers {
+				outstring += string(*p) + " / "
+			}
 		}
-		for _, p := range pointers {
-			outstring += string(*p) + " / "
-		}
-		fmt.Println(outstring)
 	}
 }
 
